3b4d8bbc2dbd6408da46dfd04c62298268317a6b
AlpacaMale <alpaca9100@gmail.com>
2024-10-01 07:10:05 +0900

    Set MongoDB connection

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,6 @@ const fakeUser = {
   username: "Nicolas",
   loggedIn: false,
 };
-
 let videos = [
   { title: "Marvel", rating: 5, comments: 2, createdAt: "2 minutes ago", views: 1, id: 0 },
   { title: "Second", rating: 5, comments: 2, createdAt: "2 minutes ago", views: 59, id: 1 },
@@ -22,4 +21,18 @@ export const getEdit = (req, res) => {
   const video = videos[id];
   res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = { title, rating: 0, comments: 0, createdAt: "2 minutes ago", views: 0, id: 3 };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
