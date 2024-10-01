b158d4e8bc96179c1196c2f1e485edf546e631cf
AlpacaMale <alpaca9100@gmail.com>
2024-10-02 07:07:11 +0900

    Update mongoose middleware

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, maxLength: 140 },
+  createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, required: true, default: 0 },
+    rating: { type: Number, required: true, default: 0 },
   },
+});
+
+videoSchema.pre("save", async function () {
+  console.log("We are about to save", this);
+  this.hashtags = this.hashtags[0].split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
